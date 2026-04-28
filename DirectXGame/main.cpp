5ec570262422ff34821fe32c9c405b8360bc31c0
夕木 夜間 <finn.yaki.0rz@gmail.com>
2026-04-28 22:22:42 +0900

    update: 04_03_確認課題_エンジンの使い方

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -8,6 +8,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	KamataEngine::Initialize(L"GC2B_08_ラ_ケツブン");
 	KamataEngine::DirectXCommon* dxCommon = KamataEngine::DirectXCommon::GetInstance();
 
+	uint32_t textureHandle_ = KamataEngine::TextureManager::Load("uvChecker.png");
+
+	KamataEngine::Sprite* sprite_ = KamataEngine::Sprite::Create(textureHandle_, {100, 50});
 
 	while (true) {
 		if (KamataEngine::Update()) {
@@ -16,8 +19,16 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 		dxCommon->PreDraw();
 
+		sprite_->PreDraw();
+
+
+		sprite_->Draw();
+
+		sprite_->PostDraw();
+
 		dxCommon->PostDraw();
 	}
+	delete sprite_;
 	KamataEngine::Finalize();
 	
 	return 0;
